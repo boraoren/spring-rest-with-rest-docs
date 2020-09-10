96b332bb64ef438ce6ac411ee35cfa826129571c
bora <boraoren@gmail.com>
2020-09-10 14:05:19 +1000

    feat(project): initialize project

---
+++ src/test/java/com/bora/spring/rest/with/restdocs/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bora.spring.rest.with.restdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
